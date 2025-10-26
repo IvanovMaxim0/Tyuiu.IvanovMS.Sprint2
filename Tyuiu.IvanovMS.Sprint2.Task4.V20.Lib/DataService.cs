21d44363351a287256e7023ef66fc46b25da66de
Tyuiu.IvanovMS@outlook.com <ivanovmaximqq@gmail.com>
2025-10-27 00:19:42 +0500

    Task4

--- Tyuiu.IvanovMS.Sprint2.Task4.V20.Lib/DataService.cs
+++ Tyuiu.IvanovMS.Sprint2.Task4.V20.Lib/DataService.cs
@@ -5,7 +5,7 @@ namespace Tyuiu.IvanovMS.Sprint2.Task4.V20.Lib
     {
         public double Calculate(double x, double y)
         {
-            double z = x + 10 > y * 2 ? x * Math.Pow((y + 1 / (x - 1)), x) : Math.Pow(x, 2) - (1 / y);
+            double z = x + 10 > y * 2 ? x * Math.Pow(((y + 1) / (x - 1)), x) : (Math.Pow(x, 2) - (1 / y));
             return Math.Round(z, 3);
         }
     }
