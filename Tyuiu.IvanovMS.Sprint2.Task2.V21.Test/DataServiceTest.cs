d39a0d4111ad00d6dc3ef5b23cc05ce5d47cfa1c
Tyuiu.IvanovMS@outlook.com <ivanovmaximqq@gmail.com>
2025-10-26 22:31:12 +0500

    Task2

---
+++ Tyuiu.IvanovMS.Sprint2.Task2.V21.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.IvanovMS.Sprint2.Task2.V21.Lib;
+namespace Tyuiu.IvanovMS.Sprint2.Task2.V21.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCheckDotInShadedArea()
+        {
+            DataService ds = new DataService();
+            int x = 3;
+            int y = 3;
+
+            bool res = ds.CheckDotInShadedArea(x, y);
+            bool wait = true;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.IvanovMS.Sprint2.Task2.V21/Program.cs
@@ -0,0 +1,33 @@
+﻿using Tyuiu.IvanovMS.Sprint2.Task2.V21.Lib;
+class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.Title = "Спринт #2 | Выолнил: Иванов М. С. | ПКТБ-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #2                                                               *");
+        Console.WriteLine("* Оператор if – полная и короткая форма записи                            *");
+        Console.WriteLine("* Задание #2                                                              *");
+        Console.WriteLine("* Вариант #21                                                             *");
+        Console.WriteLine("* Выполнил: Иванов Максим Сергеевич | ПКТБ-25-1                           *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу на, которая запрашивает целые значения с клавиатуры  *");
+        Console.WriteLine("* и вычисляет находится ли точка с координатами X,Y в заштрихованной      *");
+        Console.WriteLine("* области.                                                                *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("Введите значение х:");
+        int x = 0;
+        x = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите значение y:");
+        int y = 0;
+        y = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine(ds.CheckDotInShadedArea(x, y));
+    }
+}
